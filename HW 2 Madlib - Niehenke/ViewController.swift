2e6c13e5d5ceb5db4bbce880eeffb2216b2427c2
Adam Niehenke <a.niehenke@setonhill.edu>
2020-02-19 14:41:43 -0500

    Initial Commit

---
+++ HW 2 Madlib - Niehenke/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW 2 Madlib - Niehenke
+//
+//  Created by Adam Niehenke on 2/19/20.
+//  Copyright © 2020 Adam Niehenke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
