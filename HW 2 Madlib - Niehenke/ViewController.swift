03fb0b921f48cb98b4db25ef2b7ec8377ea43479
Adam Niehenke <a.niehenke@setonhill.edu>
2020-02-19 21:16:34 -0500

    HW 2

--- HW 2 Madlib - Niehenke/ViewController.swift
+++ HW 2 Madlib - Niehenke/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    
+    
+    @IBOutlet weak var nounOne: UITextField!
+    @IBOutlet weak var nounTwo: UITextField!
+    @IBOutlet weak var verb: UITextField!
+    @IBOutlet weak var adjective: UITextField!
+    @IBOutlet weak var adjectiveTwo: UITextField!
+    
+    
+    @IBOutlet weak var output: UITextView!
+    
+    
+    @IBAction func activate(_ sender: Any) {
+        
+        output.text = "The " + adjective.text! + " " + nounOne.text! + " may " + verb.text! + " on the lake but my " + adjectiveTwo.text! + " " + nounTwo.text! + " owns the lake."
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    @objc func dismissKeyboard(){
+        view.endEditing(true)
     }
 
 
